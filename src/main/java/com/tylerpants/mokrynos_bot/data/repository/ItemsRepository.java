d2a88d81ee488ad008dd1dc641619ebd5b431573
tyler_pants <burlakaegor777@gmail.com>
2024-12-06 22:43:08 +0700

    Items selection stable version

--- src/main/java/com/tylerpants/mokrynos_bot/data/repository/ItemsRepository.java
+++ src/main/java/com/tylerpants/mokrynos_bot/data/repository/ItemsRepository.java
@@ -1,8 +1,8 @@
 package com.tylerpants.mokrynos_bot.data.repository;
 
-import com.tylerpants.mokrynos_bot.data.model.Animal;
 import com.tylerpants.mokrynos_bot.data.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +10,12 @@ import java.util.List;
 @Repository
 public interface ItemsRepository extends JpaRepository<Item, Integer> {
 
+    @Query("from Item where animalsArr = :animalsArr and symptom.id in :symptom")
+    List<Item> findAllByAnimalsArrAndSymptoms(String animalsArr, List<Integer> symptom);
 
+    @Query("from Item where animalsArr = :animalsArr")
+    List<Item> findAllByAnimalsArr(String animalsArr);
+
+    @Query("from Item where symptom.id in :symptom")
+    List<Item> findAllBySymptom(List<Integer> symptom);
 }
